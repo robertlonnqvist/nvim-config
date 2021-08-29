da138a59ce8b72f9300288a45950988230f6e074
Robert Lönnqvist <robert.lonnqvist@gmail.com>
2021-08-29 19:38:20 +0200

    Set up symlink to vi

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@
 mkdir -p ~/.config/nvim ~/.local/bin
 rm -f ~/.config/nvim/init.vim
 ln -sf "${PWD}/init.lua" ~/.config/nvim/
-ln -sf "$(brew --prefix)/bin/nvim" ~/.local/bin/vim
+
+if command -v brew &> /dev/null; then
+  ln -sf "$(brew --prefix)/bin/nvim" ~/.local/bin/vim
+  ln -sf "$(brew --prefix)/bin/nvim" ~/.local/bin/vi
+fi
