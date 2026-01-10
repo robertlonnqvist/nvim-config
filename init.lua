778827d48346c7320542a5118b6e7b27e41c11b3
Robert Lönnqvist <robert@robertlonnqvist.com>
2026-01-10 16:06:39 +0100

    Some more tweaks

--- init.lua
+++ init.lua
@@ -4,15 +4,14 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.showmode = false
-vim.opt.termguicolors = true
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 8
 vim.opt.cursorline = true
@@ -26,6 +25,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit", "gitrebase", "markdown" },
   callback = function()
     vim.opt_local.spell = true
+    -- adds dictionary words to the Ctrl-n / Ctrl-p completion list
     vim.opt_local.complete:append("kspell")
   end,
 })
